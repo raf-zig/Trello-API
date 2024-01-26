48efe4a90e1a6114ad76364a0432ba020b84b5e3
raf-zig <ziravin@yandex.ru>
2024-01-26 22:59:29 +0500

    Initial commit

---
+++ app/controllers/column_controller.rb
@@ -0,0 +1,7 @@
+class ColumnController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    render :nothing => true, :status => :ok 
+  end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+ 
+  resources :column, only: [:index]
+  #mount_devise_token_auth_for 'User', at: 'auth'
+end
